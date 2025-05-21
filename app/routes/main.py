3d09a8de4e04e9a7e61a842b2a9ae123ff690f76
dhdev19 <dhdeveloper19@gmail.com>
2025-05-21 12:55:19 +0530

    Update main.py

--- app/routes/main.py
+++ app/routes/main.py
@@ -5,4 +5,5 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def home():
-    return render_template('home.html')
+    # return render_template('home.html')
+    return redirect("https://dhgenixmedia.ae/", code=302)
