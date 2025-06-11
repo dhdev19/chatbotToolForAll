80d7d2cde1109407a1583370318ef1e3748b04c2
dhdev19 <dhdeveloper19@gmail.com>
2025-06-11 21:37:20 +0530

    Update chatbot.py

--- app/routes/chatbot.py
+++ app/routes/chatbot.py
@@ -87,7 +87,8 @@ def add_project():
 def view_qa():
     user_id = session['user_id']
     qa_list = QuestionAnswer.get_by_user_id_and_projects(user_id)
-    return render_template('view_qa.html', qa_list=qa_list)
+    project_list = Projects.get_all_projects(user_id)
+    return render_template('view_qa.html', qa_list=qa_list, project_list=project_list)
 
 
 @bp.route('/chatbot/edit_qa/<int:qa_id>', methods=['GET', 'POST'])
